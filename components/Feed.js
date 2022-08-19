96b6205dc76e50b037f534512d7263d560e85509
kawash <kawashhabiby@hotmail.com>
2022-08-19 20:02:16 +0500

    c

--- components/Feed.js
+++ components/Feed.js
@@ -1,9 +1,16 @@
 /** @format */
 
+import { SparklesIcon } from "@heroicons/react/outline";
+
 export default function Feed() {
   return (
-    <div className="xl:ml-[23.125rem] sm:ml-[70px] border-l border-r min-w-[576px]">
-      Feed
+    <div className="xl:ml-[23.125rem] sm:ml-[73px] flex-1 border-l border-r min-w-[576px] max-w-xl">
+      <div className="">
+        <h3>Home</h3>
+        <div>
+          <SparklesIcon />
+        </div>
+      </div>
     </div>
   );
 }
