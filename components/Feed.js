9bb2c001a71ed530dc6137f9ed61ec77741fc64e
kawash <kawashhabiby@hotmail.com>
2022-08-19 19:40:19 +0500

    c

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,9 @@
 /** @format */
 
 export default function Feed() {
-  return <div className="xl:ml-[23.125rem] sm:ml-[70px]">Feed</div>;
+  return (
+    <div className="xl:ml-[23.125rem] sm:ml-[70px] border-l border-r min-w-[576px]">
+      Feed
+    </div>
+  );
 }
