b7db876d70ddfd47bfafa6c2b05c927baa4d158e
kawash <kawashhabiby@hotmail.com>
2022-08-25 01:44:42 +0500

    c

--- components/Feed.js
+++ components/Feed.js
@@ -2,17 +2,43 @@
 
 import { SparklesIcon } from "@heroicons/react/outline";
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+  const posts = [
+    {
+      id: 1,
+      name: "Mir Kawash Habibzade",
+      username: "React Developer ",
+      userImg:
+        "https://media-exp1.licdn.com/dms/image/C4D03AQH7xcKUw3Svfg/profile-displayphoto-shrink_100_100/0/1657620735523?e=1666224000&v=beta&t=ssizmeOh18nM_NQsDsbu5r7-hbOOYQ7Httw30yPguMk",
+      img: "https://images.unsplash.com/photo-1525770041010-2a1233dd8152?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      timeStamp: "2 hours ago..",
+    },
+    {
+      id: 2,
+      name: "Mir Kawash Habibzade",
+      username: "Mir Kawash ",
+      userImg:
+        "https://media-exp1.licdn.com/dms/image/C4D03AQH7xcKUw3Svfg/profile-displayphoto-shrink_100_100/0/1657620735523?e=1666224000&v=beta&t=ssizmeOh18nM_NQsDsbu5r7-hbOOYQ7Httw30yPguMk",
+      img: "https://images.unsplash.com/photo-1585250003680-b12dbff01e65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+      timeStamp: "2 hours ago..",
+    },
+  ];
+
   return (
-    <div className="xl:ml-[23.125rem] sm:ml-[73px]  flex-grow border-l border-r min-w-[576px] max-w-xl">
-      <div className="flex sticky z-50 border-b border-gray-400 py-3 px-3 top-0">
+    <div className="xl:ml-[23.125rem] sm:ml-[73px]  flex-grow border-l border-r min-w-[576px] max-w-xl ">
+      <div className="flex sticky z-50 border-b border-gray-400 py-3 px-3 top-0 bg-white">
         <h3 className="font-bold text-lg mt-1 sm:text-xl">Home</h3>
         <div className=" hoverEffect flex justify-center px-0 items-center ml-auto w-9 h-9">
           <SparklesIcon className="h-5" />
         </div>
       </div>
       <Input />
+
+      {posts.map((post) => {
+        return <Post key={post.id} post={post} />;
+      })}
     </div>
   );
 }
---
+++ components/Post.js
@@ -0,0 +1,54 @@
+/** @format */
+
+import {
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  TrashIcon,
+} from "@heroicons/react/outline";
+import React from "react";
+
+export default function Post({ post }) {
+  return (
+    <div className="flex justify-start items-start">
+      {/* Image */}
+      <img
+        src={post.userImg}
+        alt="user-img"
+        className="rounded-full h-[4rem] p-1"
+      />
+
+      {/* Right Side div */}
+      <div>
+        {/* Header */}
+        <div className="flex justify-between p-2">
+          {/* post user info */}
+          <div>
+            <h4>{post.name}</h4>
+            <span className="text-gray-500">{post.username}</span>
+            <span className="text-gray-300">{post.timeStamp}</span>
+          </div>
+
+          {/* dot Icon */}
+          <div>
+            <DotsHorizontalIcon className="h-10" />
+          </div>
+        </div>
+        {/* post Text */}
+        <p>{post.text}</p>
+
+        {/* post Image */}
+        <img src={post.img} alt="post-img" />
+
+        {/* Icons */}
+        <div>
+          <ChatIcon className="h-9" />
+          <HeartIcon className="h-9" />
+          <TrashIcon className="h-9" />
+          <ShareIcon className="h-9" />
+        </div>
+      </div>
+    </div>
+  );
+}
