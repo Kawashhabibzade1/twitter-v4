a1a9f646540ccdbc9c129559d56a0206b3fa5e84
kawash <kawashhabiby@hotmail.com>
2022-08-25 02:05:43 +0500

    new

--- components/Feed.js
+++ components/Feed.js
@@ -29,7 +29,7 @@ export default function Feed() {
   ];
 
   return (
-    <div className="xl:ml-[23.125rem] sm:ml-[73px]  flex-grow border-l border-r min-w-[576px] max-w-xl ">
+    <div className="xl:ml-[23.125rem] sm:ml-[73px]  flex-grow border-l border-r min-w-[621px] max-w-xl ">
       <div className="flex sticky z-50 border-b border-gray-400 py-3 px-3 top-0 bg-white">
         <h3 className="font-bold text-lg mt-1 sm:text-xl">Home</h3>
         <div className=" hoverEffect flex justify-center px-0 items-center ml-auto w-9 h-9">
