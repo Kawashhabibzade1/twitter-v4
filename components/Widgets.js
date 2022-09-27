7251385d02419b135cc359cbec9d5ceab3599863
kawash <kawashhabiby@hotmail.com>
2022-09-27 13:51:29 +0500

    c

--- components/Widgets.js
+++ components/Widgets.js
@@ -3,8 +3,6 @@ import { SearchIcon } from "@heroicons/react/outline";
 import { useState } from "react";
 import News from "./News";
 const widgets = ({ newsResults }) => {
-  const [articleNum, setArticleNum] = useState(3);
-
   return (
     <>
       <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
@@ -22,15 +20,11 @@ const widgets = ({ newsResults }) => {
           </div>
         </div>
         <div className="  xl:w-[75%] w-[90%] text-gray-700 space-y-3 bg-gray-100 rounded-xl  border border-gray-300 p-3">
-          <h4 className="text-bold text-xl ">What's happening?</h4>
-          {newsResults.slice(0, articleNum).map((article) => (
+          <h4 className="text-bold text-xl ">Whats happening?</h4>
+          {newsResults.map((article) => (
             <News key={article.title} article={article} />
           ))}
-          <button
-            className="text-blue-300 hover:text-blue-400"
-            onClick={() => {
-              setArticleNum(articleNum + 3);
-            }}>
+          <button className="text-blue-300 hover:text-blue-400">
             Show more...
           </button>
         </div>
