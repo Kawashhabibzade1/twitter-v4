01ed940301790894afff4d016cd2bbe6ad430615
kawash <kawashhabiby@hotmail.com>
2022-09-27 14:05:59 +0500

    c

--- components/Widgets.js
+++ components/Widgets.js
@@ -2,7 +2,9 @@
 import { SearchIcon } from "@heroicons/react/outline";
 import { useState } from "react";
 import News from "./News";
-const widgets = ({ newsResults }) => {
+const Widgets = ({ newsResults }) => {
+  const [change, setChange] = useState(1);
+
   return (
     <>
       <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
@@ -21,10 +23,12 @@ const widgets = ({ newsResults }) => {
         </div>
         <div className="  xl:w-[75%] w-[90%] text-gray-700 space-y-3 bg-gray-100 rounded-xl  border border-gray-300 p-3">
           <h4 className="text-bold text-xl ">Whats happening?</h4>
-          {newsResults.map((article) => (
+          {newsResults.slice(0, change).map((article) => (
             <News key={article.title} article={article} />
           ))}
-          <button className="text-blue-300 hover:text-blue-400">
+          <button
+            className="text-blue-300 hover:text-blue-400"
+            onClick={() => setChange(change + 3)}>
             Show more...
           </button>
         </div>
